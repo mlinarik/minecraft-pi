8d263fb0a0b7c38c9aece7d8a64fb779db0f8889
mlinarik <40384547+mlinarik@users.noreply.github.com>
2021-12-11 17:08:06 -0500

    Create plugins.sh

---
+++ plugins.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#worldedit
+wget -c https://dev.bukkit.org/projects/worldedit/files/3502099/download -O /plugins/bukkit.jar
+
+#worldguard
+wget -c https://dev.bukkit.org/projects/worldguard/files/3461546/download -O /plugins/worldguard.jar
+
+#dynmap
+wget -c https://dev.bukkit.org/projects/dynmap/files/latest -O /plugins/dynmap.jar
